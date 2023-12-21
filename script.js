61a9b9fb93b57f53bf0986d609c9397defee0656
chc13 <90738243+chc13@users.noreply.github.com>
2023-12-20 18:20:09 -0700

    added right click tracking

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //html elements
 const numClicksText = document.getElementById("numClicks");
+const numRightClicksText = document.getElementById("numRightClicks");
 const msPassedText = document.getElementById("msPassed");
 const numDoubleClicksText = document.getElementById("numDoubleClicks");
 
@@ -21,15 +22,31 @@ function updateNumClicksText() {
   numClicksText.textContent = numClicks.toString().padStart(4, "0");
 }
 
+let numRightClicks = 0;
 /* window.oncontextmenu = (e) => {
   e.preventDefault();
   console.log("right clicked");
 };
  */
 
-document.addEventListener("contextmenu", (event) => {
+/* document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
   console.log("right click detected!");
-});
+}); */
+
+document.addEventListener("contextmenu", handleRightMouseClicks);
+
+function handleRightMouseClicks(event) {
+  event.preventDefault(); //prevents context menu from showing up
+  console.log("right click detected!");
+  if (numClicks > 0) {
+    checkDoubleClick();
+  }
+  numRightClicks++;
+  numRightClicksText.textContent = numRightClicks.toString().padStart(4, "0");
+
+  restartTimer();
+}
 
 let timeInterval;
 let msCount = 0;
@@ -37,12 +54,14 @@ let msCount = 0;
 function restartTimer() {
   clearInterval(timeInterval);
   msCount = 0;
+  msPassedText.textContent = msCount.toString().padStart(4, "0");
 
   //set time interval to 1 ms
   timeInterval = setInterval(() => {
     msCount++;
     msPassedText.textContent = msCount.toString().padStart(4, "0");
   }, 1);
+  //NOTE: setInterval is INACCURATE, find another solution
 }
 
 let numDoubleClicks = 0;
